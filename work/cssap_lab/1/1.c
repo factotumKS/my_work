0ea65e2e5b56a7a9fbaf0b8b1288881856b8a186
factotumKS <1312005925@qq.com>
2018-07-24 21:12:58 +0800

    补充了CSAPP lab1的一道题

---
+++ work/cssap_lab/1/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int my_abs(int x){
+	result = x >> 31;
+	result = (result+x) ^ result
+	return x;
+}
+
+void main(){
+	int x = 1;
+	int y = -1;
+	int z = 0;
+	printf("%d %d %d\n", my_abs(x), my_abs(y), my_abs(z));
+	return;
+}
